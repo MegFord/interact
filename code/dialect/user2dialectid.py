ef9f2dd67d33da94c8f2839be53e97cd44d42bf4
Aron Culotta <aronwc@gmail.com>
2013-05-23 13:50:44 -0500

    rm dialect

--- code/dialect/user2dialectid.py
+++
@@ -1,2 +0,0 @@
-# input: user profile object (Facebook, Twitter, ...)
-# output: identifier of the predicted dialect cluster for this person
--- code/dialect/word2dialect.py
+++
@@ -1,2 +0,0 @@
-# Input: list of words
-# Output: list of list of words. For each input word, output a list of words in different "dialects"
