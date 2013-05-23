2d1e0d7e3b51b02f2f05334d410d63e99a75ffbd
Aron Culotta <aronwc@gmail.com>
2013-05-23 10:16:52 -0500

    dialect stubs

---
+++ code/dialect/user2dialectid.py
@@ -0,0 +1,2 @@
+# input: user profile object (Facebook, Twitter, ...)
+# output: identifier of the predicted dialect cluster for this person
---
+++ code/dialect/word2dialect.py
@@ -0,0 +1,2 @@
+# Input: list of words
+# Output: list of list of words. For each input word, output a list of words in different "dialects"
