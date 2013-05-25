34f623d6035a027c69cdf6fb8d00eb8b8b1c87eb
Aron Culotta <aronwc@gmail.com>
2013-05-25 12:48:21 -0500

    clustering works

--- code/style/cluster.py
+++ code/style/cluster.py
@@ -7,6 +7,7 @@ import re
 import string
 
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.cluster import MiniBatchKMeans
 
 ap = argparse.ArgumentParser(description=__doc__,
                              formatter_class=argparse.RawTextHelpFormatter)
@@ -23,6 +24,16 @@ ap.add_argument('--tfidf',
                 type=lambda s: bool(int(s)),
                 default=True,
                 help='if 1, do tfidf transform')
+ap.add_argument('--clusters',
+                metavar='CLUSTERS',
+                type=int,
+                default=10,
+                help='number of clusters')
+ap.add_argument('--seed',
+                metavar='SEED',
+                type=int,
+                default=1234567,
+                help='number of clusters')
 ap.add_argument('--test',
                 metavar='BINARY',
                 type=lambda s: bool(int(s)),
@@ -31,13 +42,26 @@ ap.add_argument('--test',
 
 args = ap.parse_args()
 
+def center2str(c, alpha, n):
+   top_indices = c.argsort()[-n:][::-1]
+   return ' '.join(['%s=%g' % (alpha[i],c[i]) for i in top_indices])
+
+def print_centers(km, alpha):
+   for i,c in enumerate(km.cluster_centers_):
+      print 'cluster',i,center2str(c, alpha, 10)
+
+def invert_dict(d):
+   return dict([(idx,s) for (s,idx) in d.iteritems()])
+
 def main():
    alpha, data = pickle.load(open(args.input, 'rb'))
+   alpha = invert_dict(alpha)
+   print alpha.keys()
    if args.tfidf:
-      tfidf = TfidfTransformer()
-      tfidf.fit(data)
-      data = tfidf.transform(data)
-   print data.getrow(0)
+      data = TfidfTransformer().fit_transform(data)
+   km = MiniBatchKMeans(n_clusters=args.clusters, random_state=args.seed)
+   km.fit(data)
+   print_centers(km, alpha)
 
 if (__name__ == '__main__'):
    if args.test:
