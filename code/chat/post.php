74a7806841516c5555f3252fcf14a61675c47118
Rachel Adler <r-adler@neiu.edu>
2013-06-10 21:44:55 -0500

    backdoor
    1.  Mohamed's latest changes that were on the CS server website.
    
    2.  old changes put back ("You logged out.." removed, and the updated
    post.php with Healthy Guru:...)
    
    3. removed first page loginForm - to login as admin use this URL
    http://cs.neiu.edu/~tempt?admin=true

--- code/chat/post.php
+++ code/chat/post.php
@@ -1,15 +1,13 @@
 <?
 session_start();
-if(isset($_SESSION['name'])){
 	$text = $_POST['text'];
 	
 	$fp = fopen("log.html", 'a');
-   if($_SESSION['name']=='admin'){
+    if($_SESSION['name']=='admin'){
 	fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>Health Guru: </b>".stripslashes(htmlspecialchars($text))."<br></div>");
     }
 	else{
 		fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>You: </b>".stripslashes(htmlspecialchars($text))."<br></div>");
 	}
 	fclose($fp);
-}
 ?>
